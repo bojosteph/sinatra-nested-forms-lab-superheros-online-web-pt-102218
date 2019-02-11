4d3337135fc4f96eb78f2da8e2a7c2ac14f2fb46
Roberto Samia <bojosteph@gmail.com>
2019-02-11 23:54:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,17 +9,14 @@ class App < Sinatra::Base
     end
     
     post '/teams' do
-      
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
-      
+
       members.each do |member_params|
         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       end
-      
       @super_heroes = SuperHero.all
-      
+
       erb :team
     end
-
 end
